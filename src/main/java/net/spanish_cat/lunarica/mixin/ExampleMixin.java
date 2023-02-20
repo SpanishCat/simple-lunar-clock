853bafb37d33a3b0a03eaa27ccc377cd56f1c338
SpanishCat <73846269+SpanishCat@users.noreply.github.com>
2023-02-20 15:06:17 +0200

    Initial commit

---
+++ src/main/java/net/spanish_cat/lunarica/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.spanish_cat.lunarica.mixin;
+
+import net.spanish_cat.lunarica.Lunarica;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Lunarica.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
